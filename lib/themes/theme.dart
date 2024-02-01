05c1036463b9ad697b34674eb7931d9bf52d19de
Ye Myo Aung <yemyo994@gmail.com>
2024-02-01 21:52:26 +0630

    added theme

---
+++ lib/themes/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStandardTheme {
+  Color get scaffoldBgColor;
+
+  Color get acitveColor;
+  Color get unselectedColor;
+  Color get bottomNavBgColor;
+
+  Color get cardColor;
+  Color get cardFgColor;
+
+  ThemeData get ref;
+
+  ThemeData get theme => ref.copyWith(
+        scaffoldBackgroundColor: scaffoldBgColor,
+        cardTheme: CardTheme(
+          shape: const RoundedRectangleBorder(),
+          color: cardFgColor,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: bottomNavBgColor,
+          selectedItemColor: acitveColor,
+          unselectedItemColor: unselectedColor,
+        ),
+        cardColor: cardColor,
+      );
+}
