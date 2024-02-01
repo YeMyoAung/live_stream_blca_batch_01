f7c52bcefad44307db109755dbfcb9c07244e925
Ye Myo Aung <yemyo994@gmail.com>
2024-02-01 21:51:33 +0630

    added result

---
+++ lib/core/models/result.dart
@@ -0,0 +1,14 @@
+import 'package:live_stream/core/errors/error.dart';
+
+class Result<T> {
+  final T? _data;
+  final GeneralError? error;
+
+  Result({
+    T? data,
+    this.error,
+  }) : _data = data;
+
+  bool get hasError => error != null;
+  T get data => _data!;
+}
