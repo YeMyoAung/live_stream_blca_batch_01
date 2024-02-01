28defb597d277a554db47e77b06cc8f732a5721b
Ye Myo Aung <yemyo994@gmail.com>
2024-02-01 21:50:08 +0630

    inject require services

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,9 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 import 'package:live_stream/firebase_options.dart';
+import 'package:live_stream/services/agora/impl/agora_guest_service.dart';
 import 'package:live_stream/services/agora/impl/agora_host_service.dart';
+import 'package:live_stream/services/auth/auth.dart';
+import 'package:live_stream/services/post/post_service.dart';
 
 GetIt Locator = GetIt.asNewInstance();
 
@@ -16,6 +20,19 @@ Future<void> setup() async {
   );
 
   Locator.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
-  final service = await AgoraHostService.instance();
-  Locator.registerLazySingleton<AgoraHostService>(() => service);
+  final agoraHostService = await AgoraHostService.instance();
+
+  Locator.registerLazySingleton<AgoraHostService>(() => agoraHostService);
+
+  final agoraGuestService = await AgoraGuestService.instance();
+
+  Locator.registerLazySingleton<AgoraGuestService>(() => agoraGuestService);
+
+  Locator.registerLazySingleton(() => AuthService());
+
+  final Dio dio = Dio();
+
+  Locator.registerLazySingleton(() => dio);
+
+  Locator.registerLazySingleton(() => PostService());
 }
